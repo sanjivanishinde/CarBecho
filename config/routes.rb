b357d4611e9c2993abbba3065db30fd4d0a8aacf
Vipul A M <vipulnsward@gmail.com>
2019-02-03 17:51:28 +0530

    Add bids

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :cars
+  resources :cars do
+    resources :bids
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_112258) do
+ActiveRecord::Schema.define(version: 2019_02_03_115911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bids", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "amount"
+    t.string "phone_number"
+    t.bigint "car_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bids_on_car_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "name"
